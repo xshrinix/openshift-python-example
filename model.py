b055f8e1bbd4c264f9f434b63b13c26010b54d23
xshrinix <shrinivasanpulyadi@gmail.com>
2024-05-10 14:56:01 +0530

    Initial Commit

---
+++ model.py
@@ -0,0 +1,45 @@
+import StatLogger
+import yaml
+from dbconfig import Dssdb
+import urllib
+from sqlalchemy import Column,  Integer, SmallInteger, String, 
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+logg = StatLogger.setup_custom_logger()
+
+try:
+    with open("config.yml", 'r') as ymlfile:
+        conf = yaml.safe_load(ymlfile)
+except Exception as ex:
+    logg.error(f"Error while reading the configuration file for driver usage reason: {ex}")
+    exit(0)
+
+if str(conf['main_setting']['database']).lower().strip() == 'mysql':
+    use_driver = 'mysql+pymysql'
+else:
+    use_driver = 'mssql+pymssql'
+
+database = create_engine("{driver}://{user}:{password}@{host}:{port}/{db}"
+                         .format(driver=use_driver, user=str(Dssdb.username).strip(),
+                                 password=urllib.parse.quote(str(Dssdb.password).strip()),
+                                 host=str(Dssdb.host).strip(), port=Dssdb.port,
+                                 db=str(Dssdb.db).strip()), pool_recycle=Dssdb.recycle, pool_pre_ping=True,
+                         pool_size=Dssdb.pool_size)
+
+sm = sessionmaker(bind=database)
+Dbsession = scoped_session(sm)
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    user_id = Column(Integer, primary_key=True, nullable=False)
+    first_name = Column(String(255))
+    last_name = Column(String(255))
+    
