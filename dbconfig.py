d3ebd23dfb9ab4821e5821cc560b6b8d09445e5c
xshrinix <shrinivasanpulyadi@gmail.com>
2024-06-28 15:47:34 +0530

    Changed the mysql host

--- dbconfig.py
+++ dbconfig.py
@@ -1,5 +1,5 @@
 class Dssdb:
-    host = "mysql.xtreme-signage-app.svc.cluster.local"
+    host =  "mysql-alpine-xtreme-signage-app.apps-crc.testing" # "mysql.xtreme-signage-app.svc.cluster.local"
     username = "root"
     password = "Admin@1234" # "Db@XtremeMedia2021"
     db = "signage"
